e37c8aec5ae94148bf681e3674119977d61425b8
dionlan <dionlan.alves@gmail.com>
2022-07-25 11:56:39 -0300

    Implementação dos detalhes do hero selecionado em component filho utilizando o @Input().

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,16 +8,11 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Wolverine'
-  };
 
   heroes = HEROES;
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
-
   }
 }
