a98a5969457e7f86aea4b40a1abcf2452e6003c4
dionlan <dionlan.alves@gmail.com>
2022-07-28 19:21:15 -0300

    Criado o interceptor do token e tratando erros de response.

--- src/app/core/interceptors/http-error.interceptor.ts
+++ src/app/core/interceptors/http-error.interceptor.ts
@@ -9,6 +9,7 @@ import {
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { MessageService } from '../services/message.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
@@ -18,12 +19,23 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((err: HttpErrorResponse) => {
+        if(!environment.production){
+          console.log('error interceptor: ', err);
+        }
+
         let errorMsg = '';
 
         if(err.error instanceof ErrorEvent){
-          errorMsg = `Error: ${err.error.message}`
+          errorMsg = `Error: ${err.error.message}`;
+
+        }else if(Array.isArray(err.error) && err.error.length){
+          errorMsg = `Error: ${err.error[0]}`;
+
+        }else if(err.error.errors){
+          errorMsg = `Error: ${err.error.errors}`;
+
         }else{
-          errorMsg = `Error Code: ${err.message}, Message: ${err.message}`
+          errorMsg = `Error Code: ${err.message}, Message: ${err.message}`;
         }
 
         this.messageService.add(errorMsg);
